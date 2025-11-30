72574444a2305f20885717f8dba43061d0699540
varrbor <varrbor@ukr.net>
2025-11-30 14:10:39 +0100

    ready to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from '@/app/ui/home.module.css';
-import Image from 'next/image';
+import styles from "@/app/ui/home.module.css";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
@@ -29,14 +28,13 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-                <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
