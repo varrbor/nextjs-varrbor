518724a7e4a658cffdd524f810db42faac21cca3
varrbor <varrbor@ukr.net>
2025-12-06 21:19:29 +0100

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,7 +17,7 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(formData: FormData): Promise<void> {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
