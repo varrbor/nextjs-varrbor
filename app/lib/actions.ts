5bcb7d2636ac8db1ffc594aec7b9b1b294514740
varrbor <varrbor@ukr.net>
2025-12-06 21:47:52 +0100

    fix types

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,7 +62,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return { message: "Database Error: Failed to Update Invoice." };
+    return ;
   }
 
   revalidatePath("/dashboard/invoices");
